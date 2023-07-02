19514f9f5f9474b4c3921d9575ee436f1a5e31db
Le Anh <leanh@Les-MacBook-Air.local>
2023-07-02 12:45:33 +0700

    change

--- js/taxi_script.js
+++ js/taxi_script.js
@@ -4,10 +4,14 @@ const data = {
     img: {
         profile_pic: "img/profilePic/1.png",
         header_bg: "img/background/pexels-daniel-absi-952670.jpg",
-        page_bg: "img/background/pexels-fwstudio-129731.jpg",
-
+        page_bg: "img/background/pexels-fwstudio-12973.jpg",
+        payment_icon: "img/icon/payment_icon.png",
     },
     payment: [
+        {
+            text: "",
+            icon: "img/payment/momo.png",
+        },
         {
             text: "",
             icon: "img/payment/vietcom.png",
@@ -16,27 +20,24 @@ const data = {
             text: "",
             icon: "img/payment/techcom.png",
         },
-        {
-            text: "",
-            icon: "img/payment/momo.png",
-        },
+       
     ],
     contact: [
         {
             text: "Liên hệ Zalo",
-            link: "",
+            link: "https://zalo.me/leanh153",
             icon: "img/icon/z.png"
 
         },
         {
             text: "Liên hệ Messenger",
-            link: "",
+            link: "https://www.messenger.com/t/100003818532953/",
             icon: "img/icon/m.png"
 
         },
         {
             text: "Follow trên Tiktok",
-            link: "",
+            link: "https://www.tiktok.com/@leanh1503",
             icon: "img/icon/t.png"
 
         },
@@ -56,18 +57,19 @@ const data = {
 
 }
 
-
+// Thêm dữ liệu hình ảnh cho trang
 $('.jumbotron').attr('style', 'background-image: url(' + data.img.header_bg + ')');
 $('body').attr('style', 'background-image: url(' + data.img.page_bg + ')');
-// Thay đổi giá trị trong trang
+$('#payment-button').attr('style', 'background-image: url('+ data.img.payment_icon +')');
 $('.profile-pic').attr('src', data.img.profile_pic);
+
+
 // Thay đổi giá trị trong html có class là contact
 for (let i = 0; i < data.contact.length; i++) {
-    console.log(i);
     $('.contact').append('<div class="col-md-4 col-sm-6 col-6">'
         + '<div class="card pulse-on-hover">'
         + '<div class="card-body ">'
-        + '<a href="' + data.contact[i].link + '" class="link-info">'
+        + '<a href="' + data.contact[i].link + '" class="link-info" target="_blank">'
         + '<img src="' + data.contact[i].icon + '" class="card-img-top rounded" alt="">'
         + '<h5 class="card-title">' + data.contact[i].text + '</h5> </a> </div></div></div>'
     )
@@ -76,9 +78,8 @@ for (let i = 0; i < data.contact.length; i++) {
 // Thay đổi giá trị trong html có class là payment
 for (let i = 0; i < data.payment.length; i++) {
     $('#payment-content').append('<div class="col-12">'
-        + '<div class="card pulse-on-hover">'
+        + '<div class="card">'
         + '<div class="card-body ">'
-        + '<a href="' + data.payment[i].link + '" class="link-info">'
         + '<img src="' + data.payment[i].icon + '" class="card-img-top rounded" alt="">'
     )
 };
