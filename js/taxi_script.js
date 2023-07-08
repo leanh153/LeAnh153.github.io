2cf58bb6464218e190a6565743656916a13588d8
Le Anh <leanh@Les-MacBook-Air.local>
2023-07-08 08:29:38 +0700

    change contact

--- js/taxi_script.js
+++ js/taxi_script.js
@@ -4,14 +4,10 @@ const data = {
     img: {
         profile_pic: "img/profilePic/1.png",
         header_bg: "img/background/pexels-daniel-absi-952670.jpg",
-        page_bg: "img/background/pexels-fwstudio-12973.jpg",
         payment_icon: "img/icon/payment_icon.png",
+        vcard: "img/vcard.png",
     },
     payment: [
-        {
-            text: "",
-            icon: "img/payment/momo.png",
-        },
         {
             text: "",
             icon: "img/payment/vietcom.png",
@@ -20,60 +16,57 @@ const data = {
             text: "",
             icon: "img/payment/techcom.png",
         },
+        {
+            text: "",
+            icon: "img/payment/momo.png",
+        },
        
     ],
-    contact: [
+    social: [
         {
             text: "Liên hệ Zalo",
             link: "https://zalo.me/leanh153",
-            icon: "img/icon/z.png"
-
+            icon: "img/icon/z.png",
+            style: "color: rgb(50,114,224);",
         },
         {
             text: "Liên hệ Messenger",
             link: "https://www.messenger.com/t/100003818532953/",
-            icon: "img/icon/m.png"
+            icon: "img/icon/m.png",
+            style: "color: rgb(50,114,224);",
 
         },
         {
             text: "Follow trên Tiktok",
             link: "https://www.tiktok.com/@leanh1503",
-            icon: "img/icon/t.png"
+            icon: "img/icon/t.png",
+            style: "color: rgb(255,255,255);",
 
         },
         {
             text: "Đăng ký Youtube",
             link: "",
-            icon: "img/icon/y.png"
+            icon: "img/icon/y.png",
+            style: "color: rgb(234,51,35);",
 
         },
+        
         {
-            text: "Quét mã để lưu danh bạ",
+            text: "Đăng ký Youtube",
             link: "",
-            icon: "img/vcard.png"
+            icon: "img/icon/f.png",
+            style: "color: rgb(234,51,35);",
 
         },
+    
     ]
 
 }
 
 // Thêm dữ liệu hình ảnh cho trang
-$('.jumbotron').attr('style', 'background-image: url(' + data.img.header_bg + ')');
-$('body').attr('style', 'background-image: url(' + data.img.page_bg + ')');
-$('#payment-button').attr('style', 'background-image: url('+ data.img.payment_icon +')');
 $('.profile-pic').attr('src', data.img.profile_pic);
+$('.jumbotron').attr('style', 'background-image: url(' + data.img.header_bg + ')');
 
-
-// Thay đổi giá trị trong html có class là contact
-for (let i = 0; i < data.contact.length; i++) {
-    $('.contact').append('<div class="col-md-4 col-sm-6 col-6">'
-        + '<div class="card pulse-on-hover">'
-        + '<div class="card-body ">'
-        + '<a href="' + data.contact[i].link + '" class="link-info" target="_blank">'
-        + '<img src="' + data.contact[i].icon + '" class="card-img-top rounded" alt="">'
-        + '<h5 class="card-title">' + data.contact[i].text + '</h5> </a> </div></div></div>'
-    )
-};
 
 // Thay đổi giá trị trong html có class là payment
 for (let i = 0; i < data.payment.length; i++) {
@@ -83,9 +76,24 @@ for (let i = 0; i < data.payment.length; i++) {
         + '<img src="' + data.payment[i].icon + '" class="card-img-top rounded" alt="">'
     )
 };
+// Thêm thành phần nút mạng xã hội trong html
+for (let i = 0; i < data.social.length; i++) {
+    $('#social-buttons').append('<span class=" fa-stack social-bg fa-2x" >'
+    +'<a class=" stretched-link" target="_blank" href="'+data.social[i].link +'"></a>'
+    +'<img class="icon-img" src="'+data.social[i].icon+'" alt="" srcset=""></span>'
+    )
+};
 // Payment
 $('#payment-content').toggle();
 $('#payment-box').on('click', function () {
     $('#payment-content').toggle();
 });
+$('.payment-button').on('click', function () {
+    $('#payment-content').toggle();
+});
 
+// Navibar custom
+$('.nav-item').on('click', function(){
+    $('.nav-item').removeClass('active');
+    $(this).addClass('active');
+})
